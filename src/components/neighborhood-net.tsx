c245573fd5b41726a0dc06df4e33c2cd4d0bd0e3
Monish <valivetimonish@gmail.com>
2026-06-29 11:39:45 +0530

    Final CivicPulse submission

--- src/components/neighborhood-net.tsx
+++ src/components/neighborhood-net.tsx
@@ -74,6 +74,7 @@ export function NeighborhoodNet() {
     const unsub = subscribeToPosts(setPosts);
     return unsub;
   }, []);
+  
 
   return (
     <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
